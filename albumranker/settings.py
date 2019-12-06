009ed5cb7fe7b74e60dcaca790658806c8a86b5e
Leonardo Rodrigues <leovictorsr@gmail.com>
2019-12-06 17:51:35 -0300

    Include project configuration and update requirements

--- albumranker/settings.py
+++ albumranker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'webpack_loader',
     'albumranker',
     'ranker'
 ]
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'albumranker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'\
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'dist/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack_stats.json'),
+    }
+}
--- ranker/urls.py
+++ ranker/urls.py
@@ -1,7 +1,10 @@
+from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='frontend/index.html')),
 ]
