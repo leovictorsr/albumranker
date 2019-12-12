d8c1c073a0f376aa3554c62d9cd1a8520ce88e7e
Leonardo Rodrigues <leovictorsr@gmail.com>
2019-12-12 19:47:27 -0300

    Add missing import

--- albumranker/views.py
+++ albumranker/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.views import View
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Ranking
 from .serializers import RankingSerializer
 from . import api_handler
